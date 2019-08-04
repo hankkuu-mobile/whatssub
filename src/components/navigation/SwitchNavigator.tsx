be7258fac5615bb2e79ed345c5d27ae938ba0c24
hyochan <dooboolab@gmail.com>
2019-08-04 18:13:00 +0900

    Added splash screen

--- src/components/navigation/SwitchNavigator.tsx
+++ src/components/navigation/SwitchNavigator.tsx
@@ -24,7 +24,7 @@ const SwitchNavigator: NavigationContainer = createSwitchNavigator(
     MainEmpty,
   },
   {
-    initialRouteName: 'HomeTabNavigator',
+    initialRouteName: 'AuthLoadingScreen',
   },
 );
 
