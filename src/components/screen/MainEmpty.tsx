b2e1b6a31444f1ace1802510724c91fd737501c6
hankkuu <hankkuu@gmail.com>
2019-08-09 03:18:32 +0900

    Update component's style

--- src/components/screen/MainEmpty.tsx
+++ src/components/screen/MainEmpty.tsx
@@ -1,6 +1,8 @@
 import { IC_ADD, IC_ADD_SERVICE } from '../../utils/Icons';
 import {
   Image,
+  Platform,
+  ScrollView,
   TouchableOpacity,
   View,
 } from 'react-native';
@@ -12,27 +14,25 @@ import styled from 'styled-components/native';
 
 const Container = styled.View`
   flex: 1;
-  padding-top: ${Constants.statusBarHeight};
+  padding-top: ${Platform.OS === 'android' ? Constants.statusBarHeight : 0};
 `;
-
 const StyledText = styled.Text`
   color: white;
   font-weight: bold;
 `;
-
 const StyledOuterEmptyBox = styled.View`
   width: 100%;
   background-color: white; 
   align-items: center;
 `;
-
 const StyledEmptyBox = styled.View`
   width: 90%;
   background-color: rgb(231,234,238);
-  margin: 10px 0px;
+  margin-top: 10px;
+  margin-bottom: 10px;
 `;
 const StyledEmptySpace = styled.Text`
-  background-color: white;
+  background-color: white; 
 `;
 
 interface Props {
@@ -44,37 +44,46 @@ function Page(props: Props) {
     <Container>
       <LinearGradient
         colors={['rgb(114,181,255)', 'rgb(62,126,255)']}
-        style={{ width: '100%', height: '33%' }}>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          <StyledText
-            style={{ marginLeft: 180, marginTop: 50 }}
-          >{getString('EMPTY_WELCOME')}</StyledText>
-          <View style={{ width: 148, height: 130 }}>
-            <TouchableOpacity testID='btn' onPress={() => {}}>
-              <Image style={{ width: '100%', height: '100%' }} source={IC_ADD}/>
-              <Image style={{
-                position: 'absolute', width: 30, height: 30, right: 13, top: 20,
-              }} source={IC_ADD_SERVICE}></Image>
+        style={{ width: '100%', height: '40%' }}>
+        <ScrollView style={{ marginTop: Platform.OS === 'ios' ? 30 : 0 }} >
+          <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+            <View style={{ justifyContent: 'center', paddingRight: 40 }}>
+              <StyledText>{getString('EMPTY_WELCOME')}</StyledText>
+            </View>
+            <Image source={IC_ADD}></Image>
+            <TouchableOpacity
+              testID='btn'
+              onPress={() => {}}
+              style={{ paddingRight: 10, marginTop: 15, position: 'absolute' }}>
+              <Image style={{ width: 30, height: 30 }} source={IC_ADD_SERVICE}></Image>
             </TouchableOpacity>
           </View>
-        </View>
-        <View style={{ alignItems: 'center' }}>
-          <StyledText>{getString('EMPTY_COMMENT1')}</StyledText>
-          <StyledText>{getString('EMPTY_COMMENT2')}</StyledText>
-        </View>
+          <View style={{ alignItems: 'flex-end', paddingRight: 110 }}>
+            <StyledText style={{ paddingRight: 40 }}>{getString('EMPTY_COMMENT1')}</StyledText>
+            <StyledText>{getString('EMPTY_COMMENT2')}</StyledText>
+          </View>
+        </ScrollView>
       </LinearGradient>
       <StyledOuterEmptyBox>
         <StyledEmptyBox style={{ height: 180 }}>
-          <View style= {{ marginTop: 20.3, marginLeft: 81.5, marginRight: 70 }}>
-            <StyledEmptySpace></StyledEmptySpace>
-          </View>
           <View style= {{
-            marginTop: 20.3, marginLeft: 122.8, marginRight: 111.2,
+            marginTop: 20.3,
+            marginLeft: 81.5,
+            marginRight: 70,
           }}>
             <StyledEmptySpace></StyledEmptySpace>
           </View>
           <View style= {{
-            marginTop: 20.3, marginLeft: 122.8, marginRight: 111.2,
+            marginTop: 20.3,
+            marginLeft: 122.8,
+            marginRight: 111.2,
+          }}>
+            <StyledEmptySpace></StyledEmptySpace>
+          </View>
+          <View style= {{
+            marginTop: 20.3,
+            marginLeft: 122.8,
+            marginRight: 111.2,
           }}>
             <StyledEmptySpace></StyledEmptySpace>
           </View>
@@ -109,7 +118,7 @@ function Page(props: Props) {
           </StyledEmptyBox>
         </StyledOuterEmptyBox>
         <StyledOuterEmptyBox>
-          <StyledEmptyBox style={{ height: 60 }}>
+          <StyledEmptyBox style={{ height: 100 }}>
           </StyledEmptyBox>
         </StyledOuterEmptyBox>
       </StyledOuterEmptyBox>
