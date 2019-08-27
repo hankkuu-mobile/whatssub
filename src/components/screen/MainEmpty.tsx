9caad316cfdc822fa1e9eaae6cebae3ffa3ced5c
hankkuu <hankkuu@gmail.com>
2019-08-27 23:13:48 +0900

    Update eslinting style

--- src/components/screen/MainEmpty.tsx
+++ src/components/screen/MainEmpty.tsx
@@ -24,17 +24,17 @@ const StyledText = styled.Text`
 `;
 const StyledOuterEmptyBox = styled.View`
   width: 100%;
-  background-color: white; 
+  background-color: white;
   align-items: center;
 `;
 const StyledEmptyBox = styled.View`
   width: 90%;
-  background-color: rgb(231,234,238);
+  background-color: rgb(231, 234, 238);
   margin-top: 10px;
   margin-bottom: 10px;
 `;
 const StyledEmptySpace = styled.Text`
-  background-color: white; 
+  background-color: white;
 `;
 
 interface Props {
@@ -54,12 +54,24 @@ function Page(props: Props) {
             <TouchableOpacity
               testID='btn'
               onPress={() => {}}
-              style={{ paddingRight: 15, marginTop: 20, position: 'absolute' }}>
-              <Image style={{ width: 24, height: 24 }} source={IC_ADD_SERVICE}></Image>
+              style={{ paddingRight: 15, marginTop: 20, position: 'absolute' }}
+            >
+              <Image
+                style={{ width: 24, height: 24 }}
+                source={IC_ADD_SERVICE}
+              ></Image>
             </TouchableOpacity>
           </View>
-          <View style={{ alignItems: 'flex-end', paddingRight: 110, marginBottom: 40 }}>
-            <StyledText style={{ paddingRight: 40 }}>{getString('EMPTY_COMMENT1')}</StyledText>
+          <View
+            style={{
+              alignItems: 'flex-end',
+              paddingRight: 110,
+              marginBottom: 40,
+            }}
+          >
+            <StyledText style={{ paddingRight: 40 }}>
+              {getString('EMPTY_COMMENT1')}
+            </StyledText>
             <StyledText>{getString('EMPTY_COMMENT2')}</StyledText>
           </View>
         </SafeAreaView>
@@ -67,60 +79,71 @@ function Page(props: Props) {
       <ScrollView>
         <StyledOuterEmptyBox>
           <StyledEmptyBox style={{ height: 180 }}>
-            <View style= {{
-              marginTop: Platform.OS === 'ios' ? 30.3 : 20.3,
-              marginLeft: 81.5,
-              marginRight: 70,
-            }}>
+            <View
+              style={{
+                marginTop: Platform.OS === 'ios' ? 30.3 : 20.3,
+                marginLeft: 81.5,
+                marginRight: 70,
+              }}
+            >
               <StyledEmptySpace></StyledEmptySpace>
             </View>
-            <View style= {{
-              marginTop: 20.3,
-              marginLeft: 122.8,
-              marginRight: 111.2,
-            }}>
+            <View
+              style={{
+                marginTop: 20.3,
+                marginLeft: 122.8,
+                marginRight: 111.2,
+              }}
+            >
               <StyledEmptySpace></StyledEmptySpace>
             </View>
-            <View style= {{
-              marginTop: 20.3,
-              marginLeft: 122.8,
-              marginRight: 111.2,
-            }}>
+            <View
+              style={{
+                marginTop: 20.3,
+                marginLeft: 122.8,
+                marginRight: 111.2,
+              }}
+            >
               <StyledEmptySpace></StyledEmptySpace>
             </View>
-            <View style= {{
-              marginTop: 20.3,
-              marginLeft: 81.5,
-              marginRight: 70,
-            }}>
+            <View
+              style={{
+                marginTop: 20.3,
+                marginLeft: 81.5,
+                marginRight: 70,
+              }}
+            >
               <StyledEmptySpace></StyledEmptySpace>
             </View>
           </StyledEmptyBox>
           <StyledOuterEmptyBox>
             <StyledEmptyBox style={{ height: 60 }}>
-              <View style= {{
-                marginTop: 20.3,
-                marginLeft: 81.5,
-                marginRight: 70,
-              }}>
+              <View
+                style={{
+                  marginTop: 20.3,
+                  marginLeft: 81.5,
+                  marginRight: 70,
+                }}
+              >
                 <StyledEmptySpace></StyledEmptySpace>
               </View>
             </StyledEmptyBox>
           </StyledOuterEmptyBox>
           <StyledOuterEmptyBox>
             <StyledEmptyBox style={{ height: 60 }}>
-              <View style= {{
-                marginTop: 20.3,
-                marginLeft: 81.5,
-                marginRight: 70,
-              }}>
+              <View
+                style={{
+                  marginTop: 20.3,
+                  marginLeft: 81.5,
+                  marginRight: 70,
+                }}
+              >
                 <StyledEmptySpace></StyledEmptySpace>
               </View>
             </StyledEmptyBox>
           </StyledOuterEmptyBox>
           <StyledOuterEmptyBox>
-            <StyledEmptyBox style={{ height: 100 }}>
-            </StyledEmptyBox>
+            <StyledEmptyBox style={{ height: 100 }}></StyledEmptyBox>
           </StyledOuterEmptyBox>
         </StyledOuterEmptyBox>
       </ScrollView>
